4332f738b3028f2368f9d1a3cc12b843ed6349a5
HyserAgg <filwes@student.chalmers.se>
2022-04-21 12:32:34 +0200

    wip

--- main.py
+++ main.py
@@ -14,10 +14,12 @@ from sklearn.pipeline import Pipeline, make_pipeline
 from sklearn.cluster import KMeans
 from sklearn.feature_selection import VarianceThreshold
 from sklearn import mixture as mx
+from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.datasets import make_blobs
+from scipy.stats.stats import pearsonr
 # Classes
 #--------------------------------------------------------------------------------------------------------#
-class VarianceFilter():
+class VarianceFilter(BaseEstimator, TransformerMixin):
     def __init__(self, thrshld):
         self.thrshld = thrshld
 
@@ -28,10 +30,12 @@ class VarianceFilter():
         X_copy = X.copy()
         X_norm = RobustScaler().fit_transform(X.copy())
         X_var = X_norm.var(axis=0)
-        X_ = X_copy[:,(X_var > self.thrshld)]
+        X_ = X_copy[:,(X_var < self.thrshld)]
+        _,m = np.shape(X_)
+        print("VF returns features:",m)
         return X_
     
-class CorrelationFilter():
+class CorrelationFilter(BaseEstimator, TransformerMixin):
     def __init__(self, thrshld):
         self.thrshld = thrshld
 
@@ -41,18 +45,18 @@ class CorrelationFilter():
     def transform(self, X, y=None):
         X_ = X.copy()
         i = 0
-        while True: 
-            _,m = np.shape(X_)
-            print(m)
-            remaining_cols = np.arange(i,m)
+        _,m = np.shape(X_)
+        while i<m: 
+            _, m = np.shape(X_)
+            corr = np.zeros(shape = m, dtype = bool)
             # Find correlation with all columns j>i
-            corr = np.ndarray(shape = np.shape(remaining_cols))
-            corr = np.array([np.correlate(X_[:,i],X_[:,j]) for j in remaining_cols]).T
-            corr[0] = 0
-            #if corr > thrshld, then this feature is updated in the mask and isn't checked for i+1
-            corr_bool = (corr > self.thrshld)
-            print(np.shape(corr_bool))
-            X_ = np.delete(X_, corr_bool, axis = 1)
+            for j in range(i+1,m):
+                c, _ = pearsonr(X_[:,i],X_[:,j])
+                corr[j] = np.abs(c) > self.thrshld
+
+            X_ = np.delete(X_, corr, axis = 1)
+            if np.mod(m-i,50)== 0:
+                print(i,m)
             i += 1
         return X_
 
@@ -63,6 +67,20 @@ class CorrelationFilter():
     # DB score: Closer to zero is preferred
 #--------------------------------------------------------------------------------------------------------#
 def main():
+
+    ## Load data ##
+    label_df, feature_df = load_SEQ_data();
+    #label_encoder = LabelEncoder()
+    #true_labels = label_encoder.fit_transform(label_df)
+    #X = gen_test_data(800, 20000, 8, 0.6)
+
+
+    X = feature_df.to_numpy() # dim = (801, 20531)
+
+    question_1(X)
+    
+
+def question_1(X):
     ## Models ## 
     max_clusters = 8
     n_inits = 5
@@ -71,62 +89,44 @@ def main():
     gmodels = [mx.GaussianMixture(n_components = i,covariance_type="full", n_init = n_inits) for i in clusters]
     models.extend(gmodels)
     clusters.extend(clusters)
-    ## Load data ##
-    label_df, feature_df = load_SEQ_data();
-    label_encoder = LabelEncoder()
-    true_labels = label_encoder.fit_transform(label_df)
-    #X = gen_test_data(800, 20000, 8, 0.6)
-
-    ## Feature pre - vis ##
+    ## Feature pre - visualisation ##
     #col_hist(feature_df,1000)
 
     ## Preprocessing ##
     preprocessor = Pipeline(steps=[
-                        ('variance_filter', VarianceFilter(3.5e-4)),
+                        ('variance_filter', VarianceFilter(0.5)),
                         ("scaler", RobustScaler()),
-                        ("correlation_filter", CorrelationFilter(0.75)),
-                        ('PCA', PCA(n_components=0.90, svd_solver = 'full')),
+                        ('PCA', PCA(n_components=0.80, svd_solver = 'full')),
     ])
-    X = feature_df.to_numpy() # dim = (801, 20531)
-    X_ = preprocessor.fit(X).transform(X)
     
+    X_ = preprocessor.fit_transform(X)
     ## Initial vizualisation ## 
     pca = preprocessor["PCA"]
-    #scree_plot(pca,0.95)
-    #pair_plot(pca, 5)
+    #scree_plot(pca,0.75)
+    pair_plot(pca, 5)
 
     ## Run clustering ##
-    #metrics_df, predicted_labels = run_clustering(X_,models, clusters)
+    metrics_df, predicted_labels = run_clustering(X_,models, clusters)
     
-    #plot_metrics(metrics_df)
+    plot_metrics(metrics_df)
 
     ## Our clustering: Names are: GaussianMixture(n_components=%),KMeans(n_clusters=%, n_init=%)
-    #model = models[2]
+    model = models[2]
     #print(model)
-    #pred_labels = predicted_labels[model]
-    #pair_plot(X_, 5, pred_labels)
+    pred_labels = predicted_labels[model]
+    pair_plot(X_, 5, pred_labels)
     ## "Best" metrics + visualize ##
     #pair_plot(X_, 5, true_labels)
 
     plt.show()
-    
-    
-    # Write function that generates data dim (., 20531) with specified amount of classes and distinctness
-    #plt.show()
-    # Vizualise our predictions and compare with true labels, the different metrics, 
-    # https://scikit-learn.org/stable/modules/clustering.html#clustering-performance-evaluation
-    # https://github.com/HCGrit/MachineLearning-iamJustAStudent/blob/master/PipelineFoundation/Pipeline_Experiment.ipynb
-    # https://towardsdatascience.com/building-a-machine-learning-pipeline-3bba20c2352b
-    # https://scikit-learn.org/stable/auto_examples/cluster/plot_kmeans_digits.html#sphx-glr-auto-examples-cluster-plot-kmeans-digits-py
-
+def question_2():
+    x = 2
 def run_clustering(X_,models, clusters):
     # Run clustering and return the metrics and the predicted labels for each model
     predicted_labels = {}
     columns  = ['Clusters','Model','S', 'DB', 'CH']
     metrics_df = pd.DataFrame(columns = columns)
     model_names = [model.__class__.__name__ for model in models]
-    no_models = len(model_names)
-    no_metrics = len(columns)-2
     shape = (len(model_names),len(columns)-2)
     metrics_df['Clusters'] = pd.Series(clusters, dtype= int)
     metrics_df['Model'] = pd.Series(model_names, dtype= "string")
@@ -153,19 +153,19 @@ def run_clustering(X_,models, clusters):
     return (metrics_df, predicted_labels)
 
 def load_SEQ_data() -> pd.DataFrame:
-    if exists('./data.h5'):
+    if exists('data/data.h5'):
         print("Hdf file accessible; reading")
-        feature_df = pd.read_hdf('./data.h5')
-        label_df = pd.read_csv('./labels.csv')
+        feature_df = pd.read_hdf('data/data.h5')
+        label_df = pd.read_csv('data/labels.csv')
         label_df = label_df["Class"]
         feature_df = feature_df.iloc[:,1:]
         return label_df, feature_df
     else:
         print("No hdf file; reading csv and creating hdf")
-        df = pd.read_csv("./data.csv")
-        label_df = pd.read_csv('./labels.csv')
+        df = pd.read_csv("data/data.csv")
+        label_df = pd.read_csv('data/labels.csv')
         label_df = label_df["Class"]
-        df.to_hdf('data.h5',key = 'df', mode='w') 
+        df.to_hdf('data/data.h5',key = 'df', mode='w') 
         df = df.iloc[:,1:]  
         return label_df, feature_df
 
@@ -183,25 +183,25 @@ def col_hist(df: pd.DataFrame, no_bins: int) -> None:
 def scree_plot(pca,thrshld):
     ratios = pca.explained_variance_ratio_
     ratio_sums = np.cumsum(ratios)
-    print(ratio_sums)
-    index = np.argmax(ratio_sums>thrshld)
+    #index = np.argmax(ratio_sums>thrshld)
     comps = np.arange(1,len(ratios)+1)
     ratios = ratios/np.max(ratios)
     plt.plot(comps, ratios)
-    plt.legend("Ratio of explained variance, div by max")
+    
     plt.plot(comps, ratio_sums)
-    plt.legend("Cumulative sum of expl. var. in %")
-    plt.vlines(index, 0,1,linestyles='dashed')
+    plt.legend(["Ratio of explained variance, div by max","Cumulative sum of expl. var. in %"])
+    #plt.vlines(index, 0,1,linestyles='dashed')
     plt.show()
+
 def pair_plot(X,no_comps, pred_labels = None):
     X_ = X.copy()
     X_ = X_[:,:no_comps]
     labels = ["PC%s" % _ for _ in range(1,no_comps+1)]
     df = pd.DataFrame(X_,columns=labels)
+    print(df)
     if not isinstance(pred_labels, type(None)):
         labels = pd.Series(labels)
         df["labels"] = pred_labels
-        print(df)
         _ = sns.pairplot(df, hue = "labels")
     else: 
         _ = sns.pairplot(df)
